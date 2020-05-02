2a1a00f9c820d6f41e09afa2f847a4f74966260c
wyj2013 <64686242+wyj2013@users.noreply.github.com>
2020-05-02 20:49:11 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,25 +1,36 @@
 /**
- * Functions are mapped to blocks using various macros in comments starting with % (e.g., //% block).
- * The most important macro "block" specifies that a block should be generated for a **exported** function.
+ * Functions are mapped to blocks using various macros 
+ * in comments starting with % (e.g., //% block).
+ * The most important macro "block" specifies that a
+ * block should be generated for a **exported** function.
  */
- 
-//% block="test" color="#AA278D" 
-
+//% block="test" color="#88ccff"
 namespace test {
 
+    //% block
+    export function helloWorld() {
 
-    //% weight=20
-    //% blockId=digital_writepin 
-    //% block="digital write pin |%tname| to |%tvalue"
-    //% tname.fieldEditor="gridpicker" 
-    //% tname.fieldOptions.columns=3 
-    //% tvalue.defl=0
-    //% tvalue.min=0
-    //% tvalue.max=1	
-    export function digital_writepin(tname: DigitalPin, tvalue: number): void {
-        pins.digitalWritePin(tname, tvalue)
     }
 
-
-
+    // note that Caml casing yields lower case
+    // block text with spaces
+    //% weight=20
+    //% blockID=analog_writepin
+    //% block="analog output|%tpins|value|%value"
+    //% tpins.fieldEditor="gridpicker"
+    //% tpins.fieldOptins.columns=3
+    export function analog_writepin(tpins: AnalogPin, value:number) :void{
+        pins.analogWritePin(tpins,tvalue)
+    }
+    //% weight=20
+    //% blockID=digital_writepin
+    //% block="digital write pin|%tname| to |% tvalue"
+    //% tpins.fieldEditor="gridpicker"
+    //% tpins.fieldOptins.columns=3
+    //% tvalue.defl=0
+    //% tvalue.min=0
+    //% tvalue.max=1
+    export function digital_writepin(tpins: DigitalPin, tvalue:number) :void{
+        pins.digitalWritePin(tname,tvalue)
+    }
 }

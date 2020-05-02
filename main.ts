cc4a06a7ac3463d77e7ff713671427e4ed488d7b
wyj2013 <64686242+wyj2013@users.noreply.github.com>
2020-05-02 21:00:23 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -6,30 +6,16 @@
  */
 //% block="test" color="#88ccff"
 namespace test {
-
- 
-
-    // note that Caml casing yields lower case
-    // block text with spaces
-    //% weight=20
-    //% blockID=analog_writepin
-    //% block="analog output|%tpins|value|%value"
-    //% tpins.fieldEditor="gridpicker"
-    //% tpins.fieldOptins.columns=3
-    export function analog_writepin(tpins: AnalogPin, tvalue:number) :void{
-        pins.analogWritePin(tpins,tvalue)
-    }
-    
-    
   
-    //% blockID=digital_writepin
-    //% block="digital write pin|%tname| to |% tvalue"
-    //% tpins.fieldEditor="gridpicker"
-    //% tpins.fieldOptins.columns=3
+    //% weight=20
+    //% blockId=digital_writepin 
+    //% block="digital write pin |%tname| to |%tvalue"
+    //% tname.fieldEditor="gridpicker" 
+    //% tname.fieldOptions.columns=3 
     //% tvalue.defl=0
     //% tvalue.min=0
-    //% tvalue.max=1
-    export function digital_writepin(tpins: DigitalPin, tvalue:number) :void{
-        pins.digitalWritePin(tname,tvalue)
+    //% tvalue.max=1	
+    export function digital_writepin(tname: DigitalPin, tvalue: number): void {
+        pins.digitalWritePin(tname, tvalue)
     }
 }
